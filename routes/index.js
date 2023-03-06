cfa487d3230915c7f25b5483268a8f1a6091b21d
Sunny Kalola <sjkalola@uwaterloo.ca>
2023-03-05 22:43:29 -0500

    added a basic user route in the backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
+
+/* Local imports */
+import { userRouter } from "./user";
 
 const app = express();
 app.use(express.json());
@@ -25,7 +28,7 @@ app.use(cors());
 
 // Need to find why we need to use this
 //  mostly to access the static images or something from the build
-app.use(express.static(path.resolve(__dirname, "../../client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Env file configuration
 const ENV_FILE = path.join(__dirname, `../.env`);
@@ -34,22 +37,29 @@ dotenv.config({ path: ENV_FILE });
 
 const port = process.env.PORT || 3001;
 
-mongoose.connect("mongodb://localhost/ecommerce", { useNewUrlParser: true });
-const db = mongoose.connection;
+// mongoose.connect("mongodb://localhost/ecommerce", { useNewUrlParser: true });
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-	console.log("Connected to MongoDB");
-});
-
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+// 	console.log("Connected to MongoDB");
+// });
 
 /* Define the Reoutes here for the backend */
+
+app.use("/user", userRouter);
+
 app.get("/api", (req, res) => {
 	res.send({ message: "Hello from the backend!" });
 });
 
 app.get("/", (req, res) => {
 	res.send({ message: "Hello from the backend!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,20 @@
-import { express } from "express";
+import express from "express";
 
-export const userRouter = express.Route(); 
+export const userRouter = express.Router();
 
-userRouter.get('/:username', async(req, res) =>{
-    let username = req.get.params.username;
-    if(username == "sunny") {
-        res.status(200).send({ username, "role": "admin" });
-    } else {
-        res.status(200).send({ username, "role": "user" });
-    }
-} )
+userRouter.get("/:username", async (req, res) => {
+	let username = req.params.username;
+	if (username == "sunny") {
+		res.status(200).send({ username, role: "admin" });
+	} else {
+		res.status(200).send({ username, role: "user" });
+	}
+});
+
+userRouter.get("/userid", async (req, res) => {
+    res.status(200).send({ username: "useradmin", role: "admin" });
+});
+
+userRouter.get("/", async (req, res) => {
+	res.status(200).send({ username: "admin", role: "admin" });
+});

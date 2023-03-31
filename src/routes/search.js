43045e8342e999d3ae433fcd6bc39788f25a57e7
Naitik Prajapati <nh2praja@uwaterloo.ca>
2023-03-31 10:21:12 -0400

    Category and product search

---
+++ src/routes/search.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Category from "../model/category"
+import Product from "../model/product"
+export const searchRouter = express.Router();
+
+searchRouter.get("/", async (req, res) => {
+    try {
+        const searchQuery = req.body
+        const response = []
+
+        const category = await Category.find({ cname: { $regex: searchQuery.search } });
+
+        const products = []
+        if (category.length > 0) {
+            const prods = await Product.find({ categoryID: category[0]._id })
+            products.push(prods)
+        } else {
+            const prods = await Product.find({ $or: [{ pname: { $regex: searchQuery.search } }, { description: { $regex: searchQuery.search } }] })
+            products.push(prods)
+        }
+        response.push({ "Products": products })
+        res.status(200).send(response)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+

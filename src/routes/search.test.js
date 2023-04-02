aa6e3bfbc8a1db3f6a9f36ba83a957ef0a0219ec
Bansi Donga <bjdonga@uwaterloo.ca>
2023-04-02 06:15:31 -0400

    Added search related testcases  for backend.

---
+++ src/routes/search.test.js
@@ -0,0 +1,27 @@
+import request from "supertest";
+import app from "../app";
+import mongoose from "mongoose";
+require("../db/mongoose");
+
+describe("Unit test cases for Search", () => {
+    test("Test 1: Get all categories and products as per search query.", async () => {
+  
+      const response = await request(app)
+        .get("/search/")
+        .send({ search: 'Personal' })
+        .expect(200);
+    });
+  
+    test("Test 2: Search query is missing", async () => {
+  
+        const response = await request(app)
+          .get("/search/")
+          .expect(400);
+      });
+    
+    afterAll((done) => {
+      mongoose.connection.close();
+      done();
+    });
+  },30000);
+  

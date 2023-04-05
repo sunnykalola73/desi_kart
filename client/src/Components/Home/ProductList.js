fcd9dc383c81552b11277d55091e55f6d2257dc4
Bansi Donga <bjdonga@uwaterloo.ca>
2023-04-05 12:12:46 -0400

    Frontend: Improved 'Add to cart' by allowing user to add products directly from product list.

--- client/src/Components/Home/ProductList.js
+++ client/src/Components/Home/ProductList.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {  Col, Image, Row } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 import { NotificationManager } from "react-notifications";
-import { RatingStar } from "rating-star";
-import {  Link, useNavigate, useParams } from "react-router-dom";
+import { Rating } from 'react-simple-star-rating'
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState();
-    const navigate = useNavigate();
-    const { id } = useParams();
-  
+  const [products, setProducts] = useState();
+  const [cartData, setCartData] = useState([]);
+  const { id } = useParams();
+
 
   const getProducts = () => {
     axios
@@ -28,31 +28,57 @@ export const ProductList = () => {
       });
   };
 
-    useEffect(() => {
-        getProducts();
-    }, [])
+  const handleAddToCart = (e, product) => {
+    e.stopPropagation();
+    let cartData = JSON.parse(localStorage.getItem("CartData")) || [];
+    let prductIndex = cartData.findIndex(p => p._id === product._id);
+    let thisProduct;
 
-   
-    return (
-        <>
-        <Header />
-            <Row>
-                {products && products.map((product) => {
-                    return ( 
-                        <Col md={4} key={product._id}>
-                            <Link className="card" to={`/description/${product._id}`} style={{cursor: "pointer"}}>
-                                <Image src={product.image} style={{height: "300px", width: "350px", margin:"5%"}} rounded className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">{product.pname}</h5>
-                                    {/* <p className="card-text">{product.description}</p> */}
-                                    $<span className="card-text">{product.price}</span>
-                                    <p><RatingStar id="123" rating={product.rating} /></p>
-                                </div>
-                            </Link>
-                        </Col>);
-                })}
+    if (prductIndex >= 0) {
+      thisProduct = cartData[prductIndex];
+      thisProduct.count = thisProduct.count + 1;
+      cartData[prductIndex] = thisProduct;
 
-            </Row>
-            <Footer/>
-        </>)
+    }
+    else {
+      thisProduct = {
+        ...product,
+        count: 1
+      }
+      cartData.push(thisProduct);
+    }
+
+    localStorage.setItem("CartData", JSON.stringify(cartData))
+    setCartData([...cartData]);
+    NotificationManager.success("This product has been added in cart.")
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
+
+  return (
+    <>
+      <Header />
+      <Row>
+        {products && products.map((product) => {
+          return (
+            <Col md={4} key={product._id}>
+              <div className="card">
+                <Image src={product.image} style={{ height: "300px", width: "350px", margin: "5%" }} rounded className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <Link to={`/description/${product._id}`}> <h5 className="card-title">{product.pname}</h5></Link>
+                  {/* <p className="card-text">{product.description}</p> */}
+                  $<span className="card-text">{product.price}</span>
+                  <p><Rating initialValue={product.rating} readonly={true} allowFraction /></p>
+                  <button className="btn btn-primary" onClick={(e) => handleAddToCart(e, product)}>Add To Cart</button>
+                </div>
+              </div>
+            </Col>);
+        })}
+
+      </Row>
+      <Footer />
+    </>)
 }

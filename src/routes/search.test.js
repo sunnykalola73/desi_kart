612b5ae3556770aa793db10072adfb128efb6e5c
Naitik Prajapati <nh2praja@uwaterloo.ca>
2023-04-06 04:12:46 -0400

    Backend unit test: Resolved bug in search and stripe payment module

--- src/routes/search.test.js
+++ src/routes/search.test.js
@@ -4,24 +4,22 @@ import mongoose from "mongoose";
 require("../db/mongoose");
 
 describe("Unit test cases for Search", () => {
-    test("Test 1: Get all categories and products as per search query.", async () => {
-  
-      const response = await request(app)
-        .get("/search/")
-        .send({ search: 'Personal' })
-        .expect(200);
-    });
-  
-    test("Test 2: Search query is missing", async () => {
-  
-        const response = await request(app)
-          .get("/search/")
-          .expect(400);
-      });
-    
-    afterAll((done) => {
-      mongoose.connection.close();
-      done();
-    });
-  },30000);
-  
+  test("Test 1: Get all categories and products as per search query.", async () => {
+
+    const response = await request(app)
+      .get("/search/Personal")
+      .expect(200);
+  });
+
+  test("Test 2: Search query is missing", async () => {
+
+    const response = await request(app)
+      .get("/search/")
+      .expect(404);
+  });
+
+  afterAll((done) => {
+    mongoose.connection.close();
+    done();
+  });
+}, 30000);
